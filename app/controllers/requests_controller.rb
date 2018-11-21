78aa38ce229de93aa5c1563f44f92c96eecb072a
Songyi Han <songyi.lee.han@gmail.com>
2018-11-20 20:25:43 -0500

    request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,13 +1,20 @@
 class RequestsController < ApplicationController
   def create
-
-    request = Request.new
+    @request = Request.new
+    @request.listing = @listing
+    @booking.user = current_user
     authorize(request)
-
   end
 
   def update
     request = Request.find(params[:id])
     authorize(request)
+  end
+
+  private
+
+  def find_listing
+    @listing = List.find(params[:listing_id])
+    authorize(@listing)
   end
 end
