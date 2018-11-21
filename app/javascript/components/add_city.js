f7f7d7c58c77f709901f6aac9b030228b2f2fedb
Tzvi Hamerman <16guitar@gmail.com>
2018-11-21 00:18:35 -0500

    updated

--- app/javascript/components/add_city.js
+++ app/javascript/components/add_city.js
@@ -1,12 +1,21 @@
 const tripField = document.querySelector('#trip-field-1');
-console.log(tripField)
 function addNewCityField() {
   const addCityButton = document.querySelector('#add-city');
   addCityButton.addEventListener('click', (event) => {
     event.preventDefault();
-    addCityButton.insertAdjacentHTML("beforebegin", tripField.innerHTML);
+    addCityButton.insertAdjacentHTML("beforebegin",
+      `<div id='trip-field-${newTripId()}' class='trip-field'>${tripField.innerHTML}</div>`
+    );
+    newTripId()
   });
 }
 
+function newTripId() {
+  const tripForms = document.querySelectorAll('.trip-field');
+  const lastTripId = tripForms[tripForms.length - 1]
+  return Number(lastTripId.id.split("-")[2]) + 1
+}
+
+newTripId()
 
 export { addNewCityField };
