79b545ed8b2f30650f8b5fec59157db628ab942d
Tzvi Hamerman <16guitar@gmail.com>
2018-11-21 15:44:42 -0500

    updated

--- app/javascript/components/add_city.js
+++ app/javascript/components/add_city.js
@@ -1,5 +1,5 @@
 // selects first trip field inputs (city, start_date, end_date)
-const tripField = document.querySelector("#trip-field-1");
+const firstTripField = document.querySelector("#trip-field-1");
 
 function addNewCityField() {
   const addCityButton = document.querySelector("#add-city");
@@ -7,10 +7,10 @@ function addNewCityField() {
 
   addCityButton.addEventListener("click", event => {
     event.preventDefault();
-    const tripId = newTripId();
-    const cityField = buildNewCityFields(tripId);
-    formInputs.append(cityField);
-    bindEventLister(cityField);
+    const newTripId = generateNewTripId();
+    const tripField = buildNewCityFields(newTripId);
+    formInputs.append(tripField);
+    bindEventLister(tripField);
   });
 }
 
@@ -18,13 +18,12 @@ function bindEventLister(element) {
   const btn = element.querySelector("button");
   btn.addEventListener("click", event => {
     event.preventDefault();
-    console.log(event.currentTarget);
     element.remove();
   });
 }
 
 //retrieves the last trip id and generates a new one by incrementing the id
-function newTripId() {
+function generateNewTripId() {
   const tripForms = document.querySelectorAll(".trip-field");
   const lastTripIdNumber = Number(
     tripForms[tripForms.length - 1].id.split("-")[2]
@@ -38,14 +37,14 @@ function buildNewCityFields(newTripId) {
   const tripNumber = Number(newTripId.split("-")[2]);
   const newDiv = document.createElement("div");
   newDiv.className = "trip-field";
-  newDiv.id = `${newTripId}`;
+  newDiv.id = newTripId;
   newDiv.innerHTML = `
     <label for="Please_insert_new_city:">Please insert city:</label>
-      <input type="text" name="city${tripNumber}" id="city${tripNumber}" placeholder="New York, Montreal.." required="required">
+      <input type="text" name="stops[${tripNumber}][city]" id="city${tripNumber}" placeholder="New York, Montreal.." required="required">
       <label for="Start_date:">Start date:</label>
-      <input type="date" name="start_date${tripNumber}" id="start_date${tripNumber}" value="2018-11-21" required="required">
+      <input type="date" name="stops[${tripNumber}][start_date]" id="start_date${tripNumber}" value="2018-11-21" required="required">
       <label for="End_date:">End date:</label>
-      <input type="date" name="end_date${tripNumber}" id="end_date${tripNumber}" value="2018-11-28" required="required">
+      <input type="date" name="stops[${tripNumber}][end_date]" id="end_date${tripNumber}" value="2018-11-28" required="required">
       <button id="delete-city-${tripNumber} class="delete-city">delete city</button>
   `;
 
