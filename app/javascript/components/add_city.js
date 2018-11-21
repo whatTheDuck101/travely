b97a95fbba02b255dee1dcaae7df0ea14a57da37
Tzvi Hamerman <16guitar@gmail.com>
2018-11-21 00:32:17 -0500

    updated

--- app/javascript/components/add_city.js
+++ app/javascript/components/add_city.js
@@ -1,19 +1,24 @@
+// selects first trip field inputs (city, start_date, end_date)
 const tripField = document.querySelector('#trip-field-1');
 function addNewCityField() {
   const addCityButton = document.querySelector('#add-city');
   addCityButton.addEventListener('click', (event) => {
     event.preventDefault();
     addCityButton.insertAdjacentHTML("beforebegin",
-      `<div id='trip-field-${newTripId()}' class='trip-field'>${tripField.innerHTML}</div>`
+      `<div id=${newTripId()} class='trip-field'>${tripField.innerHTML}</div>`
     );
     newTripId()
   });
 }
 
+
+//retrieves the last trip id and generates a new one by incrementing the id
 function newTripId() {
   const tripForms = document.querySelectorAll('.trip-field');
-  const lastTripId = tripForms[tripForms.length - 1]
-  return Number(lastTripId.id.split("-")[2]) + 1
+  const lastTripIdNumber = Number(tripForms[tripForms.length - 1].id.split("-")[2])
+  const newTripIdNumber = lastTripIdNumber + 1
+  const newTripId = `trip-field-${newTripIdNumber}`
+  return newTripId
 }
 
 newTripId()
