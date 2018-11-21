22708a8104df1a2448aaaadbedadec52caa50f89
Tzvi Hamerman <16guitar@gmail.com>
2018-11-20 23:59:03 -0500

    updated

--- app/javascript/components/add_city.js
+++ app/javascript/components/add_city.js
@@ -1,7 +1,10 @@
+const tripField = document.querySelector('#trip-field-1');
+console.log(tripField)
 function addNewCityField() {
   const addCityButton = document.querySelector('#add-city');
   addCityButton.addEventListener('click', (event) => {
-    console.log("hello")
+    event.preventDefault();
+    addCityButton.insertAdjacentHTML("beforebegin", tripField.innerHTML);
   });
 }
 
