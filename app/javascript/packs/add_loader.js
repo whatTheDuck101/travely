9cd81d1c6d010d07e11326820b4e3ad1693a9d56
Tzvi Hamerman <16guitar@gmail.com>
2018-11-28 11:16:00 -0500

    add loader to home page

--- app/javascript/packs/add_loader.js
+++ app/javascript/packs/add_loader.js
@@ -1,8 +1,6 @@
 const form = document.querySelector("#form");
-// const submitButton = submitButtons[0];
 
 function showLoader() {
-  console.log("hello from showLoader")
   var donut = document.querySelector(".donut");
   setTimeout(function(){ donut.style.display = "block" }, 220)
 }
